d95a95ffbd4b7195f52423e8f68ea35787074e26
Sakulra <dongbinzhao@outlook.com>
2024-01-31 22:53:46 +0800

    winterice vocation day1

--- DeepLearning/dataset_processing.py
+++ DeepLearning/dataset_processing.py
@@ -38,6 +38,7 @@ class Timer:
 # 通过ToTensor实例将图像数据从PIL类型变换成32位浮点数格式，
 # 并除以255使得所有像素的数值均在0～1之间
 trans = transforms.ToTensor()
+#mnist_train是n行两列的矩阵，第一列数据，第二列标签
 mnist_train = torchvision.datasets.FashionMNIST(
     root="../../dataset", train=True, transform=trans, download=True)
 mnist_test = torchvision.datasets.FashionMNIST(
@@ -46,7 +47,10 @@ mnist_test = torchvision.datasets.FashionMNIST(
 #数据集中包含的图片书
 print(len(mnist_train), len(mnist_test))
 #每张图片的大小，每张图片就相当于一个正方体横着一片一片切下来的片
+#mnist_train[0][0]表示第一张图片的数据，mnist_train[0][1]表示第一张图片的标签
+#数据集中的每一张图片都以（C，H，W）的格式存储，即(通道数，高度，宽度)
 print(mnist_train[0][0].shape)
+#print(mnist_train[0][1])#得到一个int数字
 
 def get_fashion_mnist_labels(labels:list) ->list[str]:
     """用于在数字标签索引及其文本名称之间进行转换。给0-9返回Fashion-MNIST数据集的文本标签"""
@@ -54,28 +58,48 @@ def get_fashion_mnist_labels(labels:list) ->list[str]:
                    'sandal','shirt','sneaker','bag','ankel boot']
     return [text_labels[int(i)] for i in labels]
 
-#可视化样本
+#可视化样本，法一
 def show_images(imgs,num_rows,num_cols,titles=None,scale=1.5):
     """绘制图像列表"""
+    #xscale和yscale函数的作用都是设置坐标轴的缩放类型，默认的坐标轴缩放类型为："linear"
     figsize = (num_cols*scale,num_rows*scale)
-    _,axes = d2l.plt.subplots(num_rows,num_cols,figsize=figsize)
-    #
+    _,axes = d2l.plt.subplots(num_rows,num_cols,figsize=figsize)#返回的axes是num_rows行num_cols列的矩阵
+    # print(axes)
     axes = axes.flatten()
-    for i,(ax,img) in enumerate(zip(axes,imgs)):
-        if torch.is_tensor(img):
+    # print(axes)
+    #i就是为了对图片的数字编号转换成它的真实名字
+    for i,(ax,img) in enumerate(zip(axes,imgs)):#其实省略了data这个中间变量，for i,data in enumerate(zip(axes,imgs))
+                                                                           #ax,img=data
+        if torch.is_tensor(img):#检查类型与python自带的isinstance(obj, type)效果一样，如果检查出obj是type类型返回true，否则返回false。
             #图片张量
-            ax.imshow(img.numpy())
+            ax.imshow(img.numpy())#imshow用于显示图像。
         else:
             #PIL图片
             ax.imshow(img)
+        #隐藏坐标轴
         ax.axes.get_xaxis().set_visible(False)
         ax.axes.get_yaxis().set_visible(False)
         if titles:
             ax.set_title(titles[i])
     return axes 
-    
-X,y = next(iter(data.DataLoader(mnist_train,batch_size=18)))
-show_images(X.reshape(18,28,28),2,9,titles=get_fashion_mnist_labels(y))
+#可视化样本，法二
+def show_images2(imgs,num_cols,titles):
+    # display.set_matplotlib_formats('svg')
+    d2l.use_svg_display()
+    num_cols = int(len(imgs)/num_cols)
+    _,figs = plt.subplots(num_cols,num_rows)
+    figs = figs.flatten()
+    for f,img,lbl in zip(figs,imgs,titles):#zip将对象中对应的元素打包成一个个元组，然后返回由这些元组组成的列表
+        f.imshow(img.view(28,28).numpy())#view()的作用相当于numpy中的reshape，重新定义矩阵的形状。
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+
+images,num_labels = next(iter(data.DataLoader(mnist_train,batch_size=18)))#num_labels是数字标号，其实就是做数据集的时候把相应的label英文名转换为数字
+#print(num_labels)#tensor([9, 0, 0, 3, 0, 2, 7, 2, 5, 5, 0, 9, 5, 5, 7, 9, 1, 0])
+show_images(images.reshape(18,28,28),2,9,titles=get_fashion_mnist_labels(num_labels))
 d2l.plt.show()
 
 batch_size = 256
